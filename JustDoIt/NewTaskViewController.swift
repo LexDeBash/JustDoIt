b92400cc0b5b44422a8b80180a0c071ed6108d18
Alexey Efimov <lex.efimov@gmail.com>
2021-04-13 13:25:29 +0500

    1. Work with keyboard 2. Work with text view

--- JustDoIt/NewTaskViewController.swift
+++ JustDoIt/NewTaskViewController.swift
@@ -16,13 +16,50 @@ class NewTaskViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        doneButton.isHidden = true
+        taskTextView.becomeFirstResponder()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
     }
 
     @IBAction func doneButtonPressed() {
     }
     
     @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
+    }
+}
+
+// MARK: - Text view delegate
+extension NewTaskViewController: UITextViewDelegate {
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if doneButton.isHidden {
+            textView.text.removeAll()
+            textView.textColor = .white
+            doneButton.isHidden = false
+            
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+}
+
+// MARK: - Work with keyboard
+extension NewTaskViewController {
+    @objc private func keyboardWillShow(with notification: Notification) {
+        let key = UIResponder.keyboardFrameEndUserInfoKey
+        
+        guard let keyboardFrame = notification.userInfo?[key] as? CGRect else { return }
+        bottomConstraint.constant = keyboardFrame.height
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
