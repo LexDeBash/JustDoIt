9bd41ecfd6b7cd038fc285b669aa9a97cf6fba3d
Alexey Efimov <lex.efimov@gmail.com>
2021-04-13 13:23:32 +0500

    Setup Navigation Bar

--- JustDoIt/TaskListViewController.swift
+++ JustDoIt/TaskListViewController.swift
@@ -11,9 +11,33 @@ class TaskListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
     }
 
+    private func setupNavigationBar() {
+        let fontName = "Apple SD Gothic Neo Bold"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+       
+        navBarAppearance.titleTextAttributes = [
+            .font: UIFont(name: fontName, size: 19) ?? UIFont.systemFont(ofSize: 19),
+            .foregroundColor: UIColor.white
+        ]
+        navBarAppearance.largeTitleTextAttributes = [
+            .font: UIFont(name: fontName, size: 35) ?? UIFont.systemFont(ofSize: 35),
+            .foregroundColor: UIColor.white
+        ]
+        navBarAppearance.backgroundColor = UIColor(
+            red: 97/255,
+            green: 210/255,
+            blue: 255/255,
+            alpha: 255/255
+        )
 
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
 }
 
