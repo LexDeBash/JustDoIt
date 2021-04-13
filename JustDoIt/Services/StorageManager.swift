ac8ebbf295f6e5cfb7db421cb260db4e9f6e5f90
Alexey Efimov <lex.efimov@gmail.com>
2021-04-13 15:12:11 +0500

    1. Create StorageManager 2. fetchResultsControllerMethod implementation 3. saveTask method implementation

---
+++ JustDoIt/Services/StorageManager.swift
@@ -0,0 +1,64 @@
+//
+//  StorageManager.swift
+//  JustDoIt
+//
+//  Created by Alexey Efimov on 13.04.2021.
+//
+
+import CoreData
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "JustDoIt")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private var viewContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    private init() {}
+    
+    func fetchedResultsController(entityName: String, keyForSort: String) -> NSFetchedResultsController<NSFetchRequestResult> {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        let sortDescriptor = NSSortDescriptor(key: keyForSort, ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let fetchResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: viewContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        return fetchResultsController
+    }
+    
+    func saveTask(withTitle title: String, andPriority priority: Int16) {
+        let task = Task(context: viewContext)
+        task.title = title
+        task.priority = priority
+        task.date = Date()
+        saveContext()
+    }
+    
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
